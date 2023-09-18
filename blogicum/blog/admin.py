5a9a9f2630cd731256d8ae49047b32b83b266172
Сергей Попов <sergey1404sergey@gmail.com>
2023-09-18 22:11:01 +0300

    fix after 2 review.

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 
-from blog.models import Location, Category, Post
+from blog.models import Location, Category, Post, Comment
 
-admin.site.register(Location)
-admin.site.register(Category)
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+
+@admin.register(Location)
+class LocationAmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
