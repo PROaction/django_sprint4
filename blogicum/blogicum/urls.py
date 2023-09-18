f783d8b5785ff6828dcae86082746b82d39b291a
Сергей Попов <sergey1404sergey@gmail.com>
2023-09-18 12:29:57 +0300

    1 ver.

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -11,15 +11,13 @@ urlpatterns = [
     path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='blog')),
     path('auth/', include('django.contrib.auth.urls')),
-    path(
-            'auth/registration/',
-            CreateView.as_view(
-                template_name='registration/registration_form.html',
-                form_class=UserCreationForm,
-                success_url=reverse_lazy('blog:index'),
-            ),
-            name='registration',
-        ),
+    path('auth/registration/',
+         CreateView.as_view(
+             template_name='registration/registration_form.html',
+             form_class=UserCreationForm,
+             success_url=reverse_lazy('blog:index'), ),
+         name='registration',
+         ),
 ]
 
 if settings.DEBUG:
